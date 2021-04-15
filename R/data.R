167b3af092d0848aef7b7d647cf864e5b69877b5
Pierre Roudier <pierre.roudier@gmail.com>
2021-04-16 09:53:16 +1200

    added test data

---
+++ R/data.R
@@ -0,0 +1,29 @@
+#' @include opusreader.R
+
+#' @name test_spectra.0
+#' @title Sample OPUS file containing an MIR soil spectrum
+#' @description Sample OPUS binary file. See Details section for more information.
+#' @docType data
+#' @format A binary OPUS file
+#' @details The spectrum contained in this OPUS file was collected
+#' on a soil sample from New Zealand using a Brucker FTIR spectrometer.
+#'
+#' @examples
+#' # Access the location of the ASD file using the following command
+#' fn <- opus_file()
+#' fn
+#' # This function is actually just a shorthand for
+#' fn <- system.file("extdata", "test_spectra.0", package = "opusreader")
+#' fn
+NULL
+
+#' @name opus_file
+#' @title Get location of a sample OPUS file
+#' @description Utility function that retrieves the location of the sample OPUS binary file on disc.
+#' @return a character vector storing the location of the sample OPUS file
+#' @examples
+#' fn <- opus_file()
+#' fn
+opus_file <- function() {
+  system.file("extdata", "test_spectra.0", package = "opusreader")
+}
---
+++ R/opusreader.R
@@ -0,0 +1,10 @@
+#' @include opus_read_raw.R opus_read_file.R
+
+#' @title Reads OPUS Binary Files in R.
+#'
+#' @description \code{opuseader} implements a driver for FTIR spectroscopy data collected in the OPUS binary format.
+#'
+#' @docType package
+#' @author Philipp Baumann and Pierre Roudier
+#' @name opusreader
+NULL
