60e5e51af789ec00c726547ae617c39ce51a56e7
Pierre Roudier <pierre.roudier@gmail.com>
2021-04-23 15:48:08 +1200

    Added `simple` option to simplify output and put together spectral matrix

--- R/opus_read_file.R
+++ R/opus_read_file.R
@@ -13,13 +13,16 @@
 #' code{"ScRf"} (single channel spectrum of the reference measurement),
 #' \code{"IgSm"} (interferogram of the sample measurement) and \code{"IgRf"}
 #' (interferogram of the reference measurement).
-#' @param progress Logical (default \code{TRUE}) whether a message is
+#' @param simple Logical (defaults \code{FALSE}): if set to \code{TRUE}, returns a much smaller list. The first object of that list (\code{wavenumbers}) is the wavenumbers of the first file read. The second object (\code{spectra}) is a matrix of the corresponding spectra. Especially useful when passing more than one file to the \code{file} option, for example to read a suite of spectral file directly into a matrix.
+#' @param progress Logical (defaults \code{TRUE}) whether a message is
 #' printed when an OPUS binary file is parsed into an R list entry.
 #' @param atm_comp_minus4offset Logical whether spectra after atmospheric
 #' compensation are read with an offset of \code{-4} bites from Bruker OPUS
 #' files. Default is \code{FALSE}.
-#' @usage opus_read(file, extract = "spc", progress = TRUE,
+#'
+#' @usage opus_read(file, extract = "spc", simple = FALSE, progress = TRUE,
 #' atm_comp_minus4offset = FALSE)
+#'
 #' @include opus_read_raw.R
 #' @export
 #'
@@ -28,6 +31,7 @@
 opus_read <- function(
   file,
   extract = "spc",
+  simple = FALSE,
   progress = TRUE,
   atm_comp_minus4offset = FALSE
 ) {
@@ -61,7 +65,52 @@ opus_read <- function(
   }
 
   # If there was only one file to read, we un-nest the list one level
-  if (length(file) == 1) res <- res[[1]]
+  if (length(file) == 1) {
+    res <- res[[1]]
+  } else {
+    # If a simplified output ( = spectra matrix) was requested
+    if (simple) {
+
+      if (length(extract) > 1) {
+        stop("
+             Simple output is currently only implemented for one value of the extract option.\n
+             A workaround this limitation is to use the `lapply` function, e.g.:\n\n
+             lapply(c('spc', 'ScRf'), function(x) read_opus(file, extract = x, simple = TRUE))
+             ")
+      }
+
+      # Fetch wavenumbers
+      wns <- lapply(
+        res,
+        function(x) round(x$wavenumbers, digits = 0)
+      )
+
+      # # Check all wavenumbers in collection are identical
+      # if (all(sapply(wns, identical, y = wns[[1]]))) {
+
+      specs <- lapply(
+        res,
+        function(x) {
+
+          if (extract == "spc") id <- "spc"
+          else if (extract == "ScSm") id <- "sc_sm"
+          else if (extract == "ScRf") id <- "sc_rf"
+          else if (extract == "IgSm") id <- "ig_sm"
+          else if (extract == "IgRf") id <- "ig_rf"
+
+          x[[id]]
+        })
+
+      res <- list(
+        wavenumbers = wns[[1]],
+        spectra = do.call(rbind, specs)
+      )
+
+      # } else {
+      #   stop("There is a mismatch in wavenumbers in the different files.", call. = FALSE)
+      # }
+    }
+  }
 
   return(res)
 }
